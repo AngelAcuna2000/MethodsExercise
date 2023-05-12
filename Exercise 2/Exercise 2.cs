60faee7dcd7132f70f5a6f6649a6f7cde29025de
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-12 00:46:10 -0400

    Created a program that takes user input and makes a story with it. Also wrote methods for each math operation and called them in the Main() method

---
+++ Exercise 2/Exercise 2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        public static int Add(params int[] values)
+        {
+            int sum = 0;
+            foreach (int value in values)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+        public static int Subtract(params int[] values)
+        {
+            int difference = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                difference -= values[i];
+            }
+            return difference;
+        }
+
+        public static int Multiply(params int[] values)
+        {
+            int product = 1;
+            foreach (int value in values)
+            {
+                product *= value;
+            }
+            return product;
+        }
+
+        public static int Divide(params int[] values)
+        {
+            int quotient = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                quotient /= values[i];
+            }
+            return quotient;
+        }
+
+        public static int Modulus(params int[] values)
+        {
+            int remainder = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                remainder %= values[i];
+            }
+            return remainder;
+        }
+
+        static void Main(string[] args)
+        {
+            Add(10, 20, 30);
+
+            Subtract(100, 50, 25);
+
+            Multiply(2, 3, 4);
+
+            Divide(8, 4, 2);
+
+            Modulus(7, 3, 2);
+        }
+    }
+}
---
+++ MethodsExercise/Exercise1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Takes user input
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            // Creates story from user input
+
+            Console.WriteLine($"This is the story of a brave adventurer named {name} and their loyal {animal} friend and their " +
+
+            $"quest to find the legendary lost treasure of {color} beard. {name} and their loyal companion followed a map that " +
+
+            $"they found in an old library. It showed them the way to a hidden island where {color} beard had buried his treasure. " +
+
+            $"They sailed across the sea, facing storms and pirates, until they reached the island. There, they searched for clues " +
+
+            $"and solved riddles to find the entrance to a secret cave. Inside the cave, they faced traps and monsters, until they " +
+
+            $"reached the final chamber. There, they saw a chest that glittered with gold and jewels. They had completed their " +
+
+            $"quest and found the treasure! The end.");
+        }
+    }
+}
